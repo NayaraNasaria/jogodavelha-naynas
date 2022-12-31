a0d238679f2a1c166f188721a05e09214b6f80b5
Nayara Nasaria Soares <nayaranasariasoares@gmail.com>
2022-12-31 00:33:01 -0300

    Jogo das Velinhas

--- scripts.js
+++ scripts.js
@@ -1,33 +1,90 @@
 const cellElements = document.querySelectorAll("[data-cell]");
 const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+const winningMessage = document.querySelector("[data-winning-message]");
+const restartButton = document.querySelector("[data-restart-button]");
+const winningPlayer = document.querySelector("[data-message-player]");
 
 let isCircleTurn;
 
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
 const startGame = () => {
+  isCircleTurn = false;
+
   for (const cell of cellElements) {
+    cell.classList.remove("circle");
+    cell.classList.remove("x");
+    cell.removeEventListener("click", handleClick);
     cell.addEventListener("click", handleClick, { once: true });
   }
 
-  isCircleTurn = false;
+  setBoardHoverClass();
+  winningMessage.classList.remove("show-winning-message");
+};
 
-  board.classList.add("x");
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Empate!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn
+      ? "Jogador 2 Venceu!"
+      : "Jogador 1 Venceu!";
+  }
+
+  winningMessage.classList.add("show-winning-message");
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
+};
+
+const checkForDraw = () => {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains("x") || cell.classList.contains("circle");
+  });
 };
 
 const placeMark = (cell, classToAdd) => {
   cell.classList.add(classToAdd);
 };
 
-const swapTurns = () => {
-  isCircleTurn = !isCircleTurn;
-
+const setBoardHoverClass = () => {
   board.classList.remove("circle");
   board.classList.remove("x");
 
   if (isCircleTurn) {
     board.classList.add("circle");
+    winningPlayer.innerText = "Jogador 2";
+    let img = document.querySelector("#avatar");
+    img.setAttribute("src", "jogador2.jpg");
   } else {
     board.classList.add("x");
+    winningPlayer.innerText = "Jogador 1";
+    let img = document.querySelector("#avatar");
+    img.setAttribute("src", "jogador1.jpg");
   }
+};
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+
+  setBoardHoverClass();
 };
 
 const handleClick = (e) => {
@@ -36,7 +93,19 @@ const handleClick = (e) => {
 
   placeMark(cell, classToAdd);
 
-  swapTurns();
+  const isWin = checkForWin(classToAdd);
+
+  const isDraw = checkForDraw();
+
+  if (isWin) {
+    endGame(false);
+  } else if (isDraw) {
+    endGame(true);
+  } else {
+    swapTurns();
+  }
 };
 
 startGame();
+
+restartButton.addEventListener("click", startGame);
