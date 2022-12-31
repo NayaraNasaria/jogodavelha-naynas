dd9b75b7d395756a06634be75c26c155655e5c46
Nayara Nasaria Soares <nayaranasariasoares@gmail.com>
2022-12-30 23:05:06 -0300

    Jogo das Velinhas

--- scripts.js
+++ scripts.js
@@ -1,10 +1,42 @@
 const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
 
-for (const cell of cellElements) {
-  cell.addEventListener("click", handleClick, { once: true });
-}
+let isCircleTurn;
 
-const handleClick = () => {
-  //colocar X ou circulo
-  //verificar por vitória
+const startGame = () => {
+  for (const cell of cellElements) {
+    cell.addEventListener("click", handleClick, { once: true });
+  }
+
+  isCircleTurn = false;
+
+  board.classList.add("x");
 };
+
+const placeMark = (cell, classToAdd) => {
+  cell.classList.add(classToAdd);
+};
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+
+  board.classList.remove("circle");
+  board.classList.remove("x");
+
+  if (isCircleTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+};
+
+const handleClick = (e) => {
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? "circle" : "x";
+
+  placeMark(cell, classToAdd);
+
+  swapTurns();
+};
+
+startGame();
